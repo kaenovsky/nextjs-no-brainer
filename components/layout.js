13d63f0865cba3fdd5289ed8610667b2c4d86fb4
kaen <kaen@alpogo.com>
2021-02-11 01:46:03 -0300

    - update

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = 'mkaen'
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = 'mkaen links & stuff'
 
 export default function Layout({ children, home }) {
   return (
@@ -13,7 +13,7 @@ export default function Layout({ children, home }) {
         <link rel="icon" href="/favicon.ico" />
         <meta
           name="description"
-          content="Learn how to build a personal website using Next.js"
+          content="mkaen links to social media"
         />
         <meta
           property="og:image"
--- pages/index.js
+++ pages/index.js
@@ -17,6 +17,7 @@ export default function Home() {
           <a rel="noreferrer" href='https://medium.com/@mkaen' target='_blank'><Image alt='medium' src="/medium.svg" width={30} height={30} /></a>
           <a rel="noreferrer" href='https://www.instagram.com/guybrushsays' target='_blank'><Image alt='instagram' src="/instagram.svg" width={30} height={30} /></a>
         </div>
+        <div className='flaticon'>Icons made by <a href="https://www.flaticon.com/authors/pixel-perfect" title="Pixel perfect">Pixel perfect</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
       </section>
     </Layout>
   )
