06afb4b2b9c7e852d66c12876ce2ee4f80c0bbd2
kaen <kaen@alpogo.com>
2021-04-28 19:10:20 -0300

    rm tw & ig

--- pages/index.js
+++ pages/index.js
@@ -13,13 +13,10 @@ export default function Home() {
         <div className='link-social-networks'>
           <a rel="noreferrer" href='https://www.github.com/kaenovsky' target='_blank'><Image alt='github' src="/github.svg" width={30} height={30} /></a>
           <a rel="noreferrer" href='https://www.linkedin.com/in/martinkaen' target='_blank'><Image alt='linkedin' src="/linkedin.svg" width={30} height={30} /></a>              
-          <a rel="noreferrer" href='https://www.twitter.com/guybrushsays' target='_blank'><Image alt='twitter' src="/twitter.svg" width={30} height={30} /></a>
           <a rel="noreferrer" href='https://medium.com/@mkaen' target='_blank'><Image alt='medium' src="/medium.svg" width={30} height={30} /></a>
-          <a rel="noreferrer" href='https://www.instagram.com/guybrushsays' target='_blank'><Image alt='instagram' src="/instagram.svg" width={30} height={30} /></a>
           <a rel="noreferrer" href='https://www.twitch.tv/jesuisguybrush' target='_blank'><Image alt='twitch' src="/twitch.svg" width={30} height={30} /></a>
           <a rel="noreferrer" href='https://www.bandcamp.com/kaenovsky' target='_blank'><Image alt='bandcamp' src="/bandcamp.png" width={30} height={30} /></a>
         </div>
-        <div className='flaticon'>Icons made by <a href="https://www.flaticon.com/authors/pixel-perfect" title="Pixel perfect">Pixel perfect</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
       </section>
     </Layout>
   )
